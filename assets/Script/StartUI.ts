760c4a77584b074fca1755dfb8639a817ec2c413
TomoChen <e1104421057@gm.lhu.edu.tw>
2025-02-07 00:59:31 +0800

    創建場景，並跳轉到下一個場景。
    button組件上面有個ClickEvent，把她從0改成1。
    我把轉換場景的功能放在Canvas上面，所以拉Canvas過去，選擇StartUI這個腳本裡面onStartButton功能。
    裡面會有director.loadScene(場景名稱)，這樣就能輕鬆轉換場景了。

---
+++ assets/Script/StartUI.ts
@@ -0,0 +1,20 @@
+import { _decorator, Component, Node, director } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('StartUI')
+export class StartUI extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+    //點擊UI以後會觸發的方法
+    public onStartButtonClick(){
+        director.loadScene("02-GameScene");//下一個場景的名字'
+    }
+
+}
+
+
