f0b098b6ec348ce7c0b489aadc7b248c0d1983c3
TomoChen <e1104421057@gm.lhu.edu.tw>
2025-02-07 19:22:33 +0800

    創建EnemyManager，使用Schedule計時器生成敵人。
    要注意裡面有很重要的寫法，包含Schedule中我使用箭頭函數，這是避免立即執行+閉包+導致this的指向變了。
    不過我還沒有使用物件池模式管理。

---
+++ assets/Script/EnemyManager.ts
@@ -0,0 +1,56 @@
+import { _decorator, Canvas, CCFloat, CCInteger, Component, director, instantiate, math, Node, Pool, Prefab, UITransform, Vec2, view } from 'cc';
+const { ccclass, property } = _decorator;
+//飛機的型別
+//換成number的方式，利用 EnumType as Number或者Number(Enum);
+enum EnemyType {
+    Enemy0,
+    Enemy1,
+    Enemy2,
+}
+@ccclass('EnemyManager')
+export class EnemyManager extends Component {
+    //需要飛機生成的頻率
+    //注意基本型別可以不用()，但是對象型別需要()
+    @property({ type: [CCFloat], tooltip: "Enemy0生成的頻率" })
+    enemysSpawnRate: number[] = [];
+    @property({ type: [Prefab], tooltip: "Enemy預製物們" })
+    enemyPrefabs: Prefab[] = [];
+    @property({ type: CCInteger, tooltip: "生成範圍" })
+    spawnRange: number[] = [];
+
+    protected onLoad(): void {
+        // const canvas = director.getScene().getComponentInChildren(Canvas);
+        // const viewSize = view.getDesignResolutionSize();
+        // const visibleSize = view.getVisibleSize();
+        // console.log("設計分辨率：" + viewSize);
+        // console.log("實際可見分辨機：" +visibleSize);
+    }
+    protected start(): void {
+        let n = this.enemyPrefabs.length - 1;
+        console.log(n);
+        //計時器的訂閱
+        for (let i = n; i > 0; i--) {
+            //注意，使用箭頭函數才可以避免this指向不同、立即執行造成undefined、閉包問題。
+            this.schedule(() => this.enemySpawn(i), this.enemysSpawnRate[i]);
+        }
+    }
+    protected onDestroy(): void {
+        //計時器的取消訂閱
+        this.unscheduleAllCallbacks();
+    }
+
+    enemySpawn(type: number) {
+        //生成敵人，使用const明確定義這個值不會改變，是個好習慣。
+        const enemy = instantiate(this.enemyPrefabs[type]);//生成敵人
+        const enmyHeight = enemy.getComponent(UITransform).height * 0.5;
+        //丟入父節點
+        this.node.addChild(enemy);
+        //生成的範圍。
+        const px: number = math.randomRangeInt(-this.spawnRange[type], this.spawnRange[type]);
+        const py: number = (view.getVisibleSize().height * 0.5) + enmyHeight;//乘法比除法快。
+        enemy.setPosition(px, py, 0);
+    }
+
+}
+
+
