14c4e62eb4a431a6cefc9aaedfc412eacc25eff7
TomoChen <e1104421057@gm.lhu.edu.tw>
2025-02-07 21:47:12 +0800

    讓飛機們可以超出邊界後被自動回收去EnemyPool裡面

--- assets/Script/Enemy.ts
+++ assets/Script/Enemy.ts
@@ -1,4 +1,5 @@
-import { _decorator, Animation, CCInteger, Component, Node } from 'cc';
+import { _decorator, Animation, CCInteger, Component, director, Node, UITransform, view } from 'cc';
+import { EnemyManager } from './EnemyManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('Enemy')
@@ -7,14 +8,35 @@ export class Enemy extends Component {
     speed: number = 300;
     //注意我們使用的是Animation組件，而不是Interface。
     @property({ type: Animation, tooltip: "擊落動畫" })
-    downAnim:Animation = null;
+    downAnim: Animation = null;
+    @property({ type: CCInteger, tooltip: "敵人種類，依照enemyN的N來決定。" })
+    enemyN: number = 0;
+    //敵人管理器
+    enemyManager: EnemyManager = null!;
+    myHeight: number;
+    protected onLoad(): void {
+        this.myHeight = this.node.getComponent(UITransform).height * 0.5;
+        this.enemyManager = director.getScene().getComponentInChildren(EnemyManager);
+        if (this.enemyManager == null) {
+            console.error("EnemyManager is null");
+        }
+    }
 
     update(deltaTime: number) {
         //控制移動
         const p = this.node.position;
-        const dy = this.speed * deltaTime;
-        this.node.setPosition(p.x, p.y - dy, p.z);
-
+        const dy = p.y - this.speed * deltaTime;
+        //超出螢幕就回收
+        if (dy < -view.getVisibleSize().height * 0.5 - this.myHeight) {
+            this.die();
+        }
+        this.node.setPosition(p.x, dy, p.z);
     }
+
+    die() {
+        this.enemyManager.enemyRecycle(this.node, this.enemyN);
+    }
+
 }
+
 
